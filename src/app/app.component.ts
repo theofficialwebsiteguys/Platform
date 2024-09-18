60a5e786e533c399c09d9787118b61b4bbe8c738
theofficialwebsiteguys <theofficialwebsiteguys@gmail.com>
2024-09-17 21:33:05 -0400

    09.17.2024: Feature Step Rough Draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { FeatureStepsComponent } from './feature-steps/feature-steps.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FeatureStepsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
