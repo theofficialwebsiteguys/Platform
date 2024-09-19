75671c3caa80cde8eb8b37feb4fdc4f561668f7e
Full Name <theofficialwebsiteguys@gmail.com>
2024-09-19 14:48:41 -0400

    pricing + FAQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: LandingComponent },
-    { path: 'landing', component: LandingComponent }
+    { path: 'landing', component: LandingComponent },
+    { path: 'pricing', component: PricingComponent }
 ];
