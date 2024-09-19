1e54f7d2acadf34e48a94f4d7d10b20edc3709dd
Full Name <theofficialwebsiteguys@gmail.com>
2024-09-19 12:54:18 -0400

    nav + hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { FeatureStepsComponent } from './feature-steps/feature-steps.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FeatureStepsComponent],
+  imports: [RouterOutlet, NavComponent, FeatureStepsComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
