4c49f648557cf4c6d8c9f39f9ef90864cfa93b1e
Full Name <theofficialwebsiteguys@gmail.com>
2024-09-22 15:16:34 -0400

    TP-5 + TP-8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { PricingComponent } from './pricing/pricing.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: LandingComponent },
     { path: 'landing', component: LandingComponent },
-    { path: 'pricing', component: PricingComponent }
+    { path: 'pricing', component: PricingComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];
