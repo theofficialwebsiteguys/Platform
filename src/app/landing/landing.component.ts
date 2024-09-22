aad32e29adf7bbeed9f52a7f95d6488d311cb0f1
Full Name <theofficialwebsiteguys@gmail.com>
2024-09-22 16:15:50 -0400

    TP-23 edits

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { FeatureProductsComponent } from '../feature-products/feature-products.component';
 import { FeatureStepsComponent } from '../feature-steps/feature-steps.component';
+import { FeaturePortfolioComponent } from '../feature-portfolio/feature-portfolio.component';
+import { Feature3Component } from '../feature-3/feature-3.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, HeroComponent, FeatureProductsComponent, FeatureStepsComponent],
+  imports: [CommonModule, HeroComponent, FeatureProductsComponent, FeatureStepsComponent, FeaturePortfolioComponent, Feature3Component],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
