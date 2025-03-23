3d58fa21b59c2399f3d163e53abbbd3e34f841c8
Full Name <theofficialwebsiteguys@gmail.com>
2025-03-23 14:54:31 -0400

    calender bugs

--- src/app/calendly/calendly.component.ts
+++ src/app/calendly/calendly.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var Calendly: any;
+
 @Component({
   selector: 'app-calendly',
   standalone: true,
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './calendly.component.scss'
 })
 export class CalendlyComponent {
-
+  ngAfterViewInit(): void {
+    Calendly.initInlineWidget({
+      url: 'https://calendly.com/theofficialwebsiteguys?hide_landing_page_details=1&hide_gdpr_banner=1&primary_color=b3cee3',
+      parentElement: document.querySelector('.calendly-inline-widget'),
+      prefill: {},
+      utm: {}
+    });
+  }
 }
